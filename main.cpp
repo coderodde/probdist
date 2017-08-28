ede633cab9d38d909cd9889f355ed5a15657a9d7
coderodde <rodion.efremov@cs.helsinki.fi>
2017-08-28 21:48:20 +0300

    Benchmark ready.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 #include "LinkedListProbabilityDistribution.hpp"
 #include "ProbabilityDistribution.hpp"
 #include "assert.hpp"
+#include <algorithm>
+#include <chrono>
+#include <cstdint>
 #include <iostream>
 
 using net::coderodde::util::ProbabilityDistribution;
@@ -11,40 +14,12 @@ using net::coderodde::util::BinaryTreeProbabilityDistribution;
 using net::coderodde::util::LinkedListProbabilityDistribution;
 
 static void test_all();
+static void demo();
+static void benchmark();
 
 int main() {
-    using net::coderodde::util::ArrayProbabilityDistribution;
-    using net::coderodde::util::LinkedListProbabilityDistribution;
-    
-    std::random_device rd{};
-    std::random_device::result_type seed = rd();
-    ArrayProbabilityDistribution<int> prob_dist1{seed};
-    LinkedListProbabilityDistribution<int> prob_dist2{};
-    
-    prob_dist1.add_element(1, 1.0);
-    prob_dist1.add_element(2, 1.0);
-    prob_dist1.add_element(3, 3.0);
-    
-    prob_dist2.add_element(1, 1.0);
-    prob_dist2.add_element(2, 1.0);
-    prob_dist2.add_element(3, 3.0);
-    
-    int arr1[4];
-    int arr2[4];
-    
-    for (int i = 0; i < 1000; ++i) {
-        arr1[prob_dist1.sample_element()]++;
-        arr2[prob_dist2.sample_element()]++;
-    }
-    
-    for (int i = 1; i < 4; ++i) {
-        std::cout << arr1[i] << "\n";
-    }
-    
-    for (int i = 1; i < 4; ++i) {
-        std::cout << arr2[i] << "\n";
-    }
-    
+    demo();
+    benchmark();
     test_all();
     REPORT
 }
@@ -376,4 +351,222 @@ static void test_tree() {
     
     ASSERT(dist6.size() == 5);
     ASSERT(dist1.size() == 3);
+}
+
+static void demo() {
+    std::cout << "--- Sanity demo ---\n";
+    
+    using net::coderodde::util::ArrayProbabilityDistribution;
+    using net::coderodde::util::LinkedListProbabilityDistribution;
+    using net::coderodde::util::BinaryTreeProbabilityDistribution;
+    
+    std::random_device rd{};
+    std::random_device::result_type seed = rd();
+    
+    ArrayProbabilityDistribution<int> prob_dist1{seed};
+    LinkedListProbabilityDistribution<int> prob_dist2{seed};
+    BinaryTreeProbabilityDistribution<int> prob_dist3{seed};
+    
+    prob_dist1.add_element(1, 1.0);
+    prob_dist1.add_element(2, 1.0);
+    prob_dist1.add_element(3, 3.0);
+    
+    prob_dist2.add_element(1, 1.0);
+    prob_dist2.add_element(2, 1.0);
+    prob_dist2.add_element(3, 3.0);
+    
+    prob_dist3.add_element(1, 1.0);
+    prob_dist3.add_element(2, 1.0);
+    prob_dist3.add_element(3, 3.0);
+    
+    int arr1[4] = {};
+    int arr2[4] = {};
+    int arr3[4] = {};
+    
+    for (int i = 0; i < 1000; ++i) {
+        arr1[prob_dist1.sample_element()]++;
+        arr2[prob_dist2.sample_element()]++;
+        arr3[prob_dist3.sample_element()]++;
+    }
+    
+    for (int i = 1; i < 4; ++i) {
+        std::cout << arr1[i] << " ";
+    }
+    
+    std::cout << "\n";
+    
+    for (int i = 1; i < 4; ++i) {
+        std::cout << arr2[i] << " ";
+    }
+    
+    std::cout << "\n";
+    
+    for (int i = 1; i < 4; ++i) {
+        std::cout << arr3[i] << " ";
+    }
+    
+    std::cout << "\n-------------------\n";
+}
+
+static size_t LOAD = 40 * 1000;
+static size_t SAMPLES = 40 * 1000;
+
+static void benchmark() {
+    
+    class CurrentTime {
+        std::chrono::high_resolution_clock m_clock;
+        
+    public:
+        uint64_t milliseconds() {
+            return std::chrono::duration_cast<std::chrono::milliseconds>
+            (m_clock.now().time_since_epoch()).count();
+        }
+    };
+    
+    ArrayProbabilityDistribution<int>      prob_dist1;
+    LinkedListProbabilityDistribution<int> prob_dist2;
+    BinaryTreeProbabilityDistribution<int> prob_dist3;
+    
+    std::vector<int> remove_order_vector;
+    
+    for (int i = 0; i < LOAD; ++i) {
+        remove_order_vector.push_back(i);
+    }
+    std::random_device rd;
+    std::mt19937 g(rd());
+    std::shuffle(remove_order_vector.begin(),
+                 remove_order_vector.end(),
+                 g);
+    
+    CurrentTime ct;
+    
+    //// ARRAY BASED BENCHMARK ////
+    std::cout << "ArrayProbabilityDistribution:\n";
+    
+    uint64_t add_time = 0;
+    uint64_t sample_time = 0;
+    uint64_t remove_time = 0;
+    
+    uint64_t start = ct.milliseconds();
+    
+    for (size_t i = 0; i < LOAD; ++i) {
+        prob_dist1.add_element(i, 1.0);
+    }
+    
+    uint64_t end = ct.milliseconds();
+    
+    add_time = end - start;
+    std::cout << "  add_element: " << add_time << " milliseconds.\n";
+    
+    start = ct.milliseconds();
+    
+    for (size_t i = 0; i < SAMPLES; ++i) {
+        prob_dist1.sample_element();
+    }
+    
+    end = ct.milliseconds();
+    
+    sample_time = end - start;
+    std::cout << "  sample_element: " << sample_time << " milliseconds.\n";
+    
+    start = ct.milliseconds();
+    
+    for (int element : remove_order_vector) {
+        prob_dist1.remove(element);
+    }
+    
+    end = ct.milliseconds();
+    
+    remove_time = end - start;
+    std::cout << "  remove: " << remove_time << " milliseconds.\n";
+    std::cout << "  Total: " << (add_time + sample_time + remove_time)
+              << " milliseconds.\n";
+    
+    
+    //// LINKED LIST BASED BENCHMARK ////
+    std::cout << "LinkedListProbabilityDistribution:\n";
+    
+    add_time = 0;
+    sample_time = 0;
+    remove_time = 0;
+    
+    start = ct.milliseconds();
+    
+    for (size_t i = 0; i < LOAD; ++i) {
+        prob_dist2.add_element(i, 1.0);
+    }
+    
+    end = ct.milliseconds();
+    
+    add_time = end - start;
+    std::cout << "  add_element: " << add_time << " milliseconds.\n";
+    
+    start = ct.milliseconds();
+    
+    for (size_t i = 0; i < SAMPLES; ++i) {
+        prob_dist2.sample_element();
+    }
+    
+    end = ct.milliseconds();
+    
+    sample_time = end - start;
+    std::cout << "  sample_element: " << sample_time << " milliseconds.\n";
+    
+    start = ct.milliseconds();
+    
+    for (int element : remove_order_vector) {
+        prob_dist2.remove(element);
+    }
+    
+    end = ct.milliseconds();
+    
+    remove_time = end - start;
+    std::cout << "  remove: " << remove_time << " milliseconds.\n";
+    std::cout << "  Total: " << (add_time + sample_time + remove_time)
+    << " milliseconds.\n";
+
+    //// TREE BASED BENCHMARK ////
+    
+    
+    //// LINKED LIST BASED BENCHMARK ////
+    std::cout << "BinaryTreeProbabilityDistribution:\n";
+    
+    add_time = 0;
+    sample_time = 0;
+    remove_time = 0;
+    
+    start = ct.milliseconds();
+    
+    for (size_t i = 0; i < LOAD; ++i) {
+        prob_dist3.add_element(i, 1.0);
+    }
+    
+    end = ct.milliseconds();
+    
+    add_time = end - start;
+    std::cout << "  add_element: " << add_time << " milliseconds.\n";
+    
+    start = ct.milliseconds();
+    
+    for (size_t i = 0; i < SAMPLES; ++i) {
+        prob_dist3.sample_element();
+    }
+    
+    end = ct.milliseconds();
+    
+    sample_time = end - start;
+    std::cout << "  sample_element: " << sample_time << " milliseconds.\n";
+    
+    start = ct.milliseconds();
+    
+    for (int element : remove_order_vector) {
+        prob_dist3.remove(element);
+    }
+    
+    end = ct.milliseconds();
+    
+    remove_time = end - start;
+    std::cout << "  remove: " << remove_time << " milliseconds.\n";
+    std::cout << "  Total: " << (add_time + sample_time + remove_time)
+    << " milliseconds.\n";
 }
